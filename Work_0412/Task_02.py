5d4c5af1ff1b5b1b5adc145d05d1c78d2f644dec
Dania <Dania2008@list.ru>
2023-01-02 20:52:50 +0300

    Added task_02

--- Work_0412/Task_02.py
+++ Work_0412/Task_02.py
@@ -21,3 +21,54 @@
 # 'text_code_words.txt'
 # 5a29v4s3D3d2F4g2O3i2a1
 # 1v2b2w2P3u2T1Y1y2W2Q
+
+
+import os.path
+
+
+def incode():
+    new_stroka = ""
+    pred_simb = ""
+    count = 1
+
+    if os.path.exists("text_words.txt"):
+        with open("text_words.txt") as S:
+            stroka = S.read()
+    else:
+        print("Запрашиваемого текстового файла не существует.")
+
+    with open("text_code_words.txt", "w") as rec_stroka:
+        for sled_simb in stroka: 
+            if sled_simb != pred_simb:
+                if pred_simb:
+                    new_stroka = new_stroka + str(count) + pred_simb
+                count = 1
+                pred_simb = sled_simb
+            else: 
+                count += 1 
+        new_stroka = new_stroka + str(count) + pred_simb 
+        rec_stroka.write(new_stroka)
+    return new_stroka       
+
+
+def outcode():
+    de_stroka = ""
+    count = ""
+
+    with open("text_code_words.txt") as V:
+        vs_stroka = V.read()
+        for simb in vs_stroka:
+            if simb.isdigit():
+                count = count + simb
+            else:
+                de_stroka = de_stroka + simb*int(count)
+                count = ""
+    return de_stroka
+
+
+n = input("Введите набор букв для проверки работы кодировки/раскодировки.\n")
+with open("text_words.txt", "w") as F:
+    F.write(n)
+a = incode()
+b = outcode() 
+print(b)
